fa545e965ad222135004adb78f39fe2202baef93
Your Name <jannat27120@gmail.com>
2024-12-05 18:31:24 +0600

    update Firebase configuration to use environment variables

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -6,12 +6,12 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyDGicpipbPDpoRkQcmkSv0qt2Qs9mUEUII",
-    authDomain: "winter-clothing-donation-40c9b.firebaseapp.com",
-    projectId: "winter-clothing-donation-40c9b",
-    storageBucket: "winter-clothing-donation-40c9b.firebasestorage.app",
-    messagingSenderId: "871492873232",
-    appId: "1:871492873232:web:7cc0b4b9364bf5b801ae95",
+    apiKey: import.meta.env.VITE_apiKey,
+    authDomain: import.meta.env.VITE_authDomain,
+    projectId: import.meta.env.VITE_projectId,
+    storageBucket: import.meta.env.VITE_storageBucket,
+    messagingSenderId: import.meta.env.VITE_messagingSenderId,
+    appId: import.meta.env.VITE_appId,
 };
 
 // Initialize Firebase
